e9eb1badc84b5a4060d53dd65d99fb60e27dcbd5
Cozy-Habit <sophia.precker77@gmail.com>
2026-07-18 15:55:05 +0200

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -27,8 +27,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
